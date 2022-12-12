d2f93b62fe0f1edf68e1939e3f2aac24554518e2
veselovvv <vycheslav5veselov@gmail.com>
2022-12-12 19:13:22 +0200

    Added a test for DeleteFactsUseCase

---
+++ app/src/test/java/com/veselovvv/numberfactstestapp/domain/facts/DeleteFactsUseCaseTest.kt
@@ -0,0 +1,31 @@
+package com.veselovvv.numberfactstestapp.domain.facts
+
+import com.veselovvv.numberfactstestapp.core.ErrorType
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Test
+
+class DeleteFactsUseCaseTest {
+    @Test
+    fun test_success() = runBlocking {
+        val factMapper = BaseFactDataToDomainMapper()
+        val useCase = DeleteFactsUseCase(TestFactsRepository(), BaseFactsDataToDomainMapper(factMapper))
+
+        val expected = FactsDomain.Success(listOf(), factMapper)
+        val actual = useCase.execute()
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun test_fail() = runBlocking {
+        val factMapper = BaseFactDataToDomainMapper()
+        val useCase = DeleteFactsUseCase(
+            TestFactsRepository(Exception()),
+            BaseFactsDataToDomainMapper(factMapper)
+        )
+
+        val expected = FactsDomain.Fail(ErrorType.GENERIC_ERROR)
+        val actual = useCase.execute()
+        Assert.assertEquals(expected, actual)
+    }
+}
