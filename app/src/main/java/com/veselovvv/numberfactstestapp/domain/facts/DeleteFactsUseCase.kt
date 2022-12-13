c77cf8f10dca2a1c21d96a254c3c2c13916fd520
veselovvv <vycheslav5veselov@gmail.com>
2022-12-13 20:17:43 +0200

    Use cases are based on interfaces instead of classes

--- app/src/main/java/com/veselovvv/numberfactstestapp/domain/facts/DeleteFactsUseCase.kt
+++ app/src/main/java/com/veselovvv/numberfactstestapp/domain/facts/DeleteFactsUseCase.kt
@@ -3,9 +3,13 @@ package com.veselovvv.numberfactstestapp.domain.facts
 import com.veselovvv.numberfactstestapp.data.facts.FactsDataToDomainMapper
 import com.veselovvv.numberfactstestapp.data.facts.FactsRepository
 
-class DeleteFactsUseCase(
-    private val repository: FactsRepository,
-    private val factsMapper: FactsDataToDomainMapper
-) {
-    suspend fun execute() = repository.deleteFacts().map(factsMapper)
+interface DeleteFactsUseCase {
+    suspend fun execute(): FactsDomain
+
+    class Base(
+        private val repository: FactsRepository,
+        private val factsMapper: FactsDataToDomainMapper
+    ) : DeleteFactsUseCase {
+        override suspend fun execute() = repository.deleteFacts().map(factsMapper)
+    }
 }
--- app/src/main/java/com/veselovvv/numberfactstestapp/domain/facts/FetchFactsUseCase.kt
+++ app/src/main/java/com/veselovvv/numberfactstestapp/domain/facts/FetchFactsUseCase.kt
@@ -3,9 +3,13 @@ package com.veselovvv.numberfactstestapp.domain.facts
 import com.veselovvv.numberfactstestapp.data.facts.FactsDataToDomainMapper
 import com.veselovvv.numberfactstestapp.data.facts.FactsRepository
 
-class FetchFactsUseCase(
-    private val repository: FactsRepository,
-    private val factsMapper: FactsDataToDomainMapper
-) {
-    suspend fun execute() = repository.fetchFacts().map(factsMapper)
+interface FetchFactsUseCase {
+    suspend fun execute(): FactsDomain
+
+    class Base(
+        private val repository: FactsRepository,
+        private val factsMapper: FactsDataToDomainMapper
+    ) : FetchFactsUseCase {
+        override suspend fun execute() = repository.fetchFacts().map(factsMapper)
+    }
 }
