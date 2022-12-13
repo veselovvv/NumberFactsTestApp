ce4420b7c71e6ff283d5b2f57ebc0f62ca491ecb
veselovvv <vycheslav5veselov@gmail.com>
2022-12-13 12:54:41 +0200

    Added ResourceProvider

---
+++ app/src/main/java/com/veselovvv/numberfactstestapp/core/ResourceProvider.kt
@@ -0,0 +1,12 @@
+package com.veselovvv.numberfactstestapp.core
+
+import android.content.Context
+import androidx.annotation.StringRes
+
+interface ResourceProvider {
+    fun getString(@StringRes id: Int): String
+
+    class Base(private val context: Context) : ResourceProvider {
+        override fun getString(id: Int) = context.getString(id)
+    }
+}
