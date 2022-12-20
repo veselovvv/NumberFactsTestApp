9a2fd6c4dcb07a569f563fca142aa6ad6668fc61
veselovvv <vycheslav5veselov@gmail.com>
2022-12-20 11:47:33 +0200

    Added working with network to data layer of fact feature

---
+++ app/src/main/java/com/veselovvv/numberfactstestapp/data/fact/cloud/FactService.kt
@@ -0,0 +1,10 @@
+package com.veselovvv.numberfactstestapp.data.fact.cloud
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface FactService {
+    @GET("{number}")
+    suspend fun fetchFact(@Path("number") number: Int): ResponseBody
+}
