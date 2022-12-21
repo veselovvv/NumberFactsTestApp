34616526e5c42b1dd779ebc87362ad1a64ed9e07
veselovvv <vycheslav5veselov@gmail.com>
2022-12-21 10:41:39 +0200

    Added presentation layer of fact feature

--- app/src/main/java/com/veselovvv/numberfactstestapp/presentation/main/MainFragment.kt
+++ app/src/main/java/com/veselovvv/numberfactstestapp/presentation/main/MainFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.veselovvv.numberfactstestapp.databinding.FragmentMainBinding
 import com.veselovvv.numberfactstestapp.presentation.core.BaseFragment
+import com.veselovvv.numberfactstestapp.presentation.fact.FactViewModel
 import com.veselovvv.numberfactstestapp.presentation.facts.FactsAdapter
 import com.veselovvv.numberfactstestapp.presentation.facts.FactsViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainFragment : BaseFragment<FragmentMainBinding>() {
     private val factsViewModel: FactsViewModel by viewModels()
+    private val factViewModel: FactViewModel by viewModels() // TODO?
 
     override fun setupViewBinding(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentMainBinding.inflate(inflater, container, false)
@@ -41,6 +44,26 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
 
         binding.deleteHistoryImageView.setOnClickListener {
             factsViewModel.deleteFacts()
+        }
+
+        binding.getFactButton.setOnClickListener {
+            // TODO change to context dialog or something
+            if (binding.numberEditText.text?.isEmpty() == true)
+                Toast.makeText(requireContext(), "The field is empty", Toast.LENGTH_SHORT).show()
+            else {
+                //TODO add validation + enabling/disabling buttons etc
+                //TODO sort list of facts from new to old
+                factViewModel.observe(this) { factUi ->
+                    factUi.map {
+                        factsViewModel.fetchFacts() // reload list of facts from database if success
+                    }
+                    factUi.map(binding.progressLayout.root)
+                    factUi.map(binding.failLayout.root, binding.failLayout.errorMessageTextView)
+                }
+
+                val enteredNumber = binding.numberEditText.text.toString().toInt()
+                factViewModel.fetchFact(enteredNumber)
+            }
         }
     }
 }
